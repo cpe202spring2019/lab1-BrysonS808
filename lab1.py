49f2e1d7482c47075750a363629d679350d4b3ab
BrysonS808 <49254265+BrysonS808@users.noreply.github.com>
2019-04-10 18:23:41 -0700

    I made these

--- lab1.py
+++ lab1.py
@@ -1,15 +1,73 @@
 
 def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
-   If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+	"""finds the max of a list of numbers and returns the value (not the index)
+	If int_list is empty, returns None. If list is None, raises ValueError"""
+	if int_list == None:
+		raise ValueError
+	if len(int_list) == 0:
+		return None
+	else:
+		max = int_list[0]
+		for x in (int_list):
+			if x > max:
+				max = x
+	return max
 
 def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
-   pass
+	"""recursively reverses a list of numbers and returns the reversed list
+	If list is None, raises ValueError"""
+	if int_list == None:
+		raise ValueError
+	if len(int_list) == 0:
+		return []
+	else:
+		return[int_list.pop()] + reverse_rec(int_list)
 
 def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
-   pass
+	"""searches for target in int_list[low..high] and returns index if found
+	If target is not found returns None. If list is None, raises ValueError """
+	if int_list == None:
+		raise ValueError
+	midpoint = int(((high - low)/2)+low)
+	'''print()
+	print("midpoint", midpoint, 'value', int_list[midpoint])
+	print('list', int_list[low:high])
+	print('low', low, 'high', high)
+	print('midpoint', midpoint, 'len', len(int_list))
+	print("len", len(int_list[low:high]))'''
+	if int_list[midpoint] == target:
+		return midpoint
+	elif len(int_list[low:high]) == 1:
+		#print('true none')
+		return None
+	elif int_list[midpoint] < target:
+		#print('higher')
+		return bin_search(target, midpoint, len(int_list), int_list)
+	elif int_list[midpoint] > target:
+		#print('lower')
+		return bin_search(target, low, midpoint, int_list)
+	else:
+		return None
+
+list1 = [1,23, 42, 4, 2, 9]
+list2 = [1,2,3,4,5,6,7,98]
+list3 = [1, 4, 6, 7, 12, 45]
+
+def tester():
+	'''print(max_list_iter(list1))
+	print(max_list_iter([]))
+	print()
+	print(reverse_rec([1,2,3,4,5]))
+	print(reverse_rec([2,34,56,2,35,3]))
+	print(reverse_rec([]))
+	#print(reverse_rec(None))
+	print()
+	#print(bin_search(None))
+	print(bin_search(4, 0, len(list2), list2))
+	print(bin_search(-1, 0, len(list3), list3))
+	print(bin_search(70, 0, len(list3), list3))
+	print(bin_search(10, 0, len(list2), list2))'''
+	pass
+
+if __name__ == "__main__":
+    tester()
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -1,5 +1,9 @@
-aimport unittest
+import unittest
 from lab1 import *
+
+list1 = [1,23, 42, 4, 2, 9]
+list2 = [1,2,3,4,5,6,7,98]
+list3 = [1, 4, 6, 7, 12, 45]
 
  # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
@@ -9,17 +13,34 @@ class TestLab1(unittest.TestCase):
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
+        ''' These tests cases test to make sure that the real max number
+            shows up'''
+        self.assertEqual(max_list_iter(list1), 42)
+        self.assertEqual(max_list_iter(list2), 98)
+        self.assertEqual(max_list_iter(list3), 45)
 
     def test_reverse_rec(self):
+        ''' tests to see if the function properly reverses the lists '''
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+        self.assertEqual(reverse_rec(list1), [9,2,4,42,23,1])
+        self.assertEqual(reverse_rec(list2), [98,7,6,5,4,3,2,1])
+        self.assertEqual(reverse_rec(list3), [45,12,7,6,4,1])
+        self.assertEqual(reverse_rec([]), [])
+        with self.assertRaises(ValueError):
+            reverse_rec(None)
 
     def test_bin_search(self):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
         self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+        ''' tests to make sure that the function runs and checks what happens
+            if the number isn't in the list'''
+        self.assertEqual(bin_search(4, 0, len(list2), list2), 3)
+        self.assertEqual(bin_search(-1, 0, len(list3), list3), None)
+        self.assertEqual(bin_search(7, 0, len(list3), list3), 3)
+        self.assertEqual(bin_search(98, 0, len(list2), list2), 7)
+
 
 if __name__ == "__main__":
         unittest.main()
-
-    
